2bee3de034507ffbf58229d60a79928e28506b21
Edgar Lopez <lopez_e55@outlook.com>
2023-11-17 16:14:59 -0800

    Renamed folder

--- Prework.py
+++ Prework.py
@@ -1,5 +1,6 @@
 # Question 1
-# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. 
+# The first line of the code has been defined as below.
 
     # def hello_name(user_name):
 
